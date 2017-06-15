c03d7cedf67b6b99cb3686fd109628008b957ed6
iOS-School-1 <iOS-School-1@iMac-John.local>
2017-06-15 21:57:03 +0300

    Initial Commit

---
+++ OCMockExample/AVGTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  AVGTestViewController.h
+//  OCMockExample
+//
+//  Created by iOS-School-1 on 15.06.17.
+//  Copyright © 2017 Paul Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVGTestViewController : UIViewController
+
+@end
---
+++ Podfile
@@ -0,0 +1,3 @@
+target :OCMockExampleTests do
+  pod 'OCMock'
+end
